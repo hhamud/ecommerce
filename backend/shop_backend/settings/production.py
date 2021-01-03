26a34e1660d262f29e1ad34b29b142b77812518f
user <hamza.h.hamud@gmail.com>
2021-01-03 21:36:51 +0000

    cloudinary add-on

--- backend/shop_backend/settings/production.py
+++ backend/shop_backend/settings/production.py
@@ -33,8 +33,8 @@ CORS_ALLOWED_ORIGINS = [
 
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.environ['CLOUD_NAME'],
-    'API_KEY': os.environ['CLOUD_API_KEY'],
-    'API_SECRET': os.environ['CLOUD_API_SECRET'],
+    'API_KEY': os.environ['API_KEY'],
+    'API_SECRET': os.environ['API_SECRET'],
 }
 
 
