87d3d97805b945406a87e2a7f300935c7c79d3b7
user <hamza.h.hamud@gmail.com>
2021-01-04 01:54:35 +0000

    completed address model

--- backend/shop_api/models.py
+++ backend/shop_api/models.py
@@ -2,31 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-class User(AbstractUser):
-    firstname = models.CharField(max_length=20)
-    lastname = models.CharField(max_length=20)
-    phone_number = models.CharField(max_length=11)
-    email_address = models.EmailField()
- 
-
-    def __str__(self):
-        return self.username
-
-    
-
-class Address(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    home_number = models.CharField(max_length=5)
-    street = models.CharField(max_length=20)
-    area = models.CharField(max_length=20)
-    city = models.CharField(max_length=20)
-    post_code = models.CharField(max_length=10)
-    country = models.CharField(max_length=20)
-
-    def __str__(self):
-        return '%s %s %s %s %s %s' % (self.home_number, self.street, self.area, self.city, self.post_code, self.country)
-
-
 class Size(models.TextChoices):
     EXTRA_SMALL = 'XS'
     SMALL = 'S'
@@ -42,6 +17,38 @@ class Catagory(models.TextChoices):
     HAT = 'HAT'
 
 
+class Addresschoices(models.TextChoices):
+    SHIPPING_ADDRESS = 'S'
+    BILLING_ADDRESS = 'B'
+
+
+class User(AbstractUser):
+    firstname = models.CharField(max_length=20)
+    lastname = models.CharField(max_length=20)
+    phone_number = models.CharField(max_length=11)
+    email_address = models.EmailField()
+
+    def __str__(self):
+        return self.username
+
+
+class Address(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    home_number = models.CharField(max_length=5)
+    street = models.CharField(max_length=20)
+    area = models.CharField(max_length=20)
+    city = models.CharField(max_length=20)
+    post_code = models.CharField(max_length=10)
+    country = models.CharField(max_length=20)
+    address_type = models.CharField(
+        max_length=1, choices=Addresschoices.choices)
+    default_address = models.BooleanField(default=False)
+
+
+    def __str__(self):
+        return '%s %s %s %s %s %s' % (self.home_number, self.street, self.area, self.city, self.post_code, self.country)
+
+
 class Product(models.Model):
     name = models.CharField(max_length=50)
     price = models.DecimalField(max_digits=8, decimal_places=2)
@@ -55,3 +62,7 @@ class Product(models.Model):
 
     def __str__(self):
         return '%s %s' % (self.name, self.price)
+
+
+class Order(models.Model):
+    pass
